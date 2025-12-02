784abc21e3edbef04abdde3313cb346c9be02c83
hayoung <hayohio@gmail.com>
2025-12-02 16:29:02 +0900

    의존성 주입 학습

--- sp1/src/main/java/org/zerock/controller/HelloController.java
+++ sp1/src/main/java/org/zerock/controller/HelloController.java
@@ -4,19 +4,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.zerock.service.HelloService;
+
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Controller
 @Log4j2
+@RequiredArgsConstructor
 public class HelloController {
 
-	@Autowired
-	private HelloService helloService;
+
+	private final HelloService helloService;
 	
-	@GetMapping("/hello")
-	public String hello() {
-		log.info("--------hello--------");
-		return null;
-	}
+	
+	
 	
 }
+
+
+/*
+의존성 주입 방식
+1. 필드 주입
+	@Autowired
+
+2. 생성자 주입
+	public HelloController(HelloService helloService) {
+	this.HelloService = helloService;
+	
+3. 생성자 주입_Lombok
+	@RequiredArgsConstructor
+ */
---
+++ sp1/src/test/java/org/zerock/db/DBTests.java
@@ -0,0 +1,25 @@
+package org.zerock.db;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import lombok.extern.log4j.Log4j2;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j2
+public class DBTests {
+	
+	@Autowired
+	private javax.sql.DataSource dataSource;
+	
+	@Test
+	public void testConnection() {
+		log.info("-------------------");
+		log.info(dataSource);
+		log.info("-------------------");
+	}
+
+}
